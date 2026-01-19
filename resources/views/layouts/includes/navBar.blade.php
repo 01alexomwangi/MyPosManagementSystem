ab3158e57f8c35dcfc5722365a4ded670f84e615
Alex <01alexomwangi@gmail.com>
2026-01-19 17:31:48 +0300

    Pos First commit

---
+++ resources/views/layouts/includes/navBar.blade.php
@@ -0,0 +1,20 @@
+<a href="#" data-toggle="modal" data-target="#staticBackdrop" class="btn btn-outline rounded-pill"><i class="fa fa-list"></i></a>
+<a href="#" class="btn btn-outline rounded-pill"><i class="fa fa-user"></i>Users</a>
+<a href="#" class="btn btn-outline rounded-pill"><i class="fa fa-box"></i>Products</a>
+<a href="#" class="btn btn-outline rounded-pill"><i class="fa fa-laptop"></i>Cashier</a>
+<a href="#" class="btn btn-outline rounded-pill"><i class="fa fa-file"></i>Reports</a>
+<a href="#" class="btn btn-outline rounded-pill"><i class="fa fa-money-bill"></i>Transactions</a>
+<a href="#" class="btn btn-outline rounded-pill"><i class="fa fa-chart"></i>Suppliers</a> 
+<a href="#" class="btn btn-outline rounded-pill"><i class="fa fa-users-group"></i>Customers</a>
+<a href="#" class="btn btn-outline rounded-pill"><i class="fa fa-user-group"></i>incomming</a>
+<style>
+    .btn-outline{
+         border-color:#008BBB;
+         color:#008BBB;
+    }
+
+     .btn-outline:hover{
+         background-color:#008BBB;
+         color:#fff;
+    }
+</style>
